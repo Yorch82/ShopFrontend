ad9a9c29375d7c9345a110b21fc3ad2c63df8045
Yorch82 <jcampo1982@outlook.com>
2023-08-28 20:44:46 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { useSelector } from "react-redux";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
+import { useMemo } from "react";
 
 function App() {
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Routes>
+          {/* <Route path="/" element={} /> */}
+        </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 }
